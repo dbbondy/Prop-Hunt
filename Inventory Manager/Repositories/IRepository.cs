6e47c70646ce951ab031e6c04fff1c17419261db
Dan <db_bondy@hotmail.com>
2016-01-18 16:33:25 -0800

    Adding a base IRepository file for all of the data access layer to implement.

---
+++ Inventory Manager/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Manager.Repositories
+{
+    interface IRepository<T>
+    {
+        IEnumerable<T> getList { get; }
+
+        void add(T item);
+
+        T update(T item);
+
+        void delete(T item);
+    }
+}
