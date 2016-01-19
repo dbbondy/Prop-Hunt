51b1c8aa47f41663e70cf6d080b78b41724a953f
Dan <db_bondy@hotmail.com>
2016-01-18 19:20:02 -0800

    Fixing an issue where the DB connection did not have a Close() call. Now utilises the 'using' statement for auto resource management.

--- Inventory Manager/Utility/DbCreateUtil.cs
+++ Inventory Manager/Utility/DbCreateUtil.cs
@@ -15,56 +15,56 @@ namespace Inventory_Manager.Utility
 
             SQLiteConnection.CreateFile("InventoryDB.sqlite");
 
-            SQLiteConnection dbConnection = new SQLiteConnection("Data Source=InventoryDB.sqlite;Version=3;");
-            dbConnection.Open();
-
-            using (var transaction = dbConnection.BeginTransaction())
+            using (var dbConnection = new SQLiteConnection("Data Source=InventoryDB.sqlite;Version=3;"))
             {
-                #region create prefix table
-                using (var command = new SQLiteCommand(dbConnection))
+                using (var transaction = dbConnection.BeginTransaction())
                 {
-                    command.CommandText = "CREATE TABLE IF NOT EXISTS prefix" + 
-                                            "(prefix_id INTEGER PRIMARY KEY ASC, prefix_name TEXT, prefix_desc TEXT)";
-                    command.ExecuteNonQuery();
-                }
-                #endregion
-
-                #region insert core prefix data
-                using (var command = new SQLiteCommand(dbConnection))
-                {
-                    command.CommandText = "INSERT OR IGNORE INTO prefix (prefix_name, prefix_desc) VALUES('COS-', 'Costumes')";
-                    command.ExecuteNonQuery();
-                }
-
-                using (var command = new SQLiteCommand(dbConnection))
-                {
-                    command.CommandText = "INSERT OR IGNORE INTO prefix (prefix_name, prefix_desc) VALUES('PR-', 'Props')";
-                    command.ExecuteNonQuery();
-                }
-                using (var command = new SQLiteCommand(dbConnection))
-                {
-                    command.CommandText = "INSERT OR IGNORE INTO prefix (prefix_name, prefix_desc) VALUES('SP-', 'Set-Pieces')";
-                    command.ExecuteNonQuery();
-                }
-
-                #endregion
-
-                #region verify there are at least 3 rows in the prefix table
-                using (var command = new SQLiteCommand(dbConnection))
-                {
-                    command.CommandText = "SELECT COUNT(*) from prefix";
-                    using (var reader = command.ExecuteReader())
+                    #region create prefix table
+                    using (var command = new SQLiteCommand(dbConnection))
                     {
-                        while (reader.Read())
+                        command.CommandText = "CREATE TABLE IF NOT EXISTS prefix" +
+                                                "(prefix_id INTEGER PRIMARY KEY ASC, prefix_name TEXT, prefix_desc TEXT)";
+                        command.ExecuteNonQuery();
+                    }
+                    #endregion
+
+                    #region insert core prefix data
+                    using (var command = new SQLiteCommand(dbConnection))
+                    {
+                        command.CommandText = "INSERT OR IGNORE INTO prefix (prefix_name, prefix_desc) VALUES('COS-', 'Costumes')";
+                        command.ExecuteNonQuery();
+                    }
+
+                    using (var command = new SQLiteCommand(dbConnection))
+                    {
+                        command.CommandText = "INSERT OR IGNORE INTO prefix (prefix_name, prefix_desc) VALUES('PR-', 'Props')";
+                        command.ExecuteNonQuery();
+                    }
+                    using (var command = new SQLiteCommand(dbConnection))
+                    {
+                        command.CommandText = "INSERT OR IGNORE INTO prefix (prefix_name, prefix_desc) VALUES('SP-', 'Set-Pieces')";
+                        command.ExecuteNonQuery();
+                    }
+
+                    #endregion
+
+                    #region verify there are at least 3 rows in the prefix table
+                    using (var command = new SQLiteCommand(dbConnection))
+                    {
+                        command.CommandText = "SELECT COUNT(*) from prefix";
+                        using (var reader = command.ExecuteReader())
                         {
-                            Debug.Assert(reader.GetInt32(0) < 3, "Missing Inventory Prefixes!"); // if there are not three rows after attempting to create the core data, throw an error to let us know.
+                            while (reader.Read())
+                            {
+                                Debug.Assert(reader.GetInt32(0) < 3, "Missing Inventory Prefixes!"); // if there are not three rows after attempting to create the core data, throw an error to let us know.
+                            }
                         }
                     }
+
+                    #endregion
+
+                    transaction.Commit();
                 }
-
-                #endregion
-
-                transaction.Commit();
             }
         }
         #endregion
