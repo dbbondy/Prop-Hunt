a26281752b0feb41df9c893d3927a2cafe43abf1
Dan <db_bondy@hotmail.com>
2016-01-18 17:20:42 -0800

    Removing all unnecessary 'using's

--- Inventory Manager/Domain/Prefix.cs
+++ Inventory Manager/Domain/Prefix.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Inventory_Manager.Domain
+﻿namespace Inventory_Manager.Domain
 {
     class Prefix
     {
--- Inventory Manager/Repositories/PrefixRepository.cs
+++ Inventory Manager/Repositories/PrefixRepository.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Inventory_Manager.Domain;
 
 namespace Inventory_Manager.Repositories
